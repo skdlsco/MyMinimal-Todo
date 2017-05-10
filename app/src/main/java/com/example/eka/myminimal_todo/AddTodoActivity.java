158eeb5a03f45499a1cf7419bfdcb10c91a01792
Eka <nicnic1234@naver.com>
2017-05-10 16:58:38 +0900

    first commit

---
+++ app/src/main/java/com/example/eka/myminimal_todo/AddTodoActivity.java
@@ -0,0 +1,13 @@
+package com.example.eka.myminimal_todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTodoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_todo);
+    }
+}
---
+++ app/src/main/java/com/example/eka/myminimal_todo/SettingActivity.java
@@ -0,0 +1,30 @@
+package com.example.eka.myminimal_todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
+public class SettingActivity extends AppCompatActivity{
+    CheckBox mcheckBox;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        mcheckBox = (CheckBox) findViewById(R.id.mcheckbox);
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.setting_layout);
+        linearLayout.setOnClickListener(checked);
+    }
+    View.OnClickListener checked = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(mcheckBox.isChecked()){
+                mcheckBox.setChecked(false);
+            }else {
+                mcheckBox.setChecked(true);
+            }
+        }
+    };
+
+}
